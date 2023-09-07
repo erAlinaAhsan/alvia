c29d9f8fd217498f845680e8139e3f20c2dd05e4
erAlinaAhsan <alina.ahsan13@gmail.com>
2023-09-07 23:35:37 +0530

    Post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,28 +1,28 @@
-<!DOCTYPE html>
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
+<x-layout>
+@include ('_posts-header')
 
-<body>
-     @foreach($posts as $post)
-    <article>
-  <h1>
-    <a href="/posts/{{$post->slug}}">
-      {!! $post->title!!}
-    </a>
-  </h1>
-  <p>
 
-    By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+  @if ($posts->count())
+  <x-posts-grid :posts="$posts"/>
+  @else
+  <p class="text-center">No post yet. Please check back later.</p>
       
-    {{$post->category->name}}</a>
-  </p>
+  @endif
 
-<div>
-  {{$post->excerpt}}
-</div>
+</main>
+</x-layout>
 
 
-    </article>
-    @endforeach 
 
-</body>
+
+
+
+
+
+
+
+
+
+
+
