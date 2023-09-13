41e94ac24bc8dc92af438472de3a15018bf14476
erAlinaAhsan <alina.ahsan13@gmail.com>
2023-09-14 00:29:16 +0530

    mailchimp started

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,21 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 
+
+
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us14'
+    ]);
+    $response = $mailchimp->lists->addListMember('24d95df14e', [
+        'email_address' => 'alinahasn@iul.ac.in',
+        'status' => 'subscribed'
+    ]);
+    dd($response);
+});
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
