a8dbc5ec1f6c22d4f6fe225e9eff1f855e831d9e
erAlinaAhsan <alina.ahsan13@gmail.com>
2023-09-21 14:42:59 +0530

    Blog

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -10,6 +10,7 @@
                     <x-form.input name="thumbnail" type="file" :value="old('thumbnail', $post->thumbnail)" />
                 </div>
                 <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="" class="rounded-xl" width="100">
+
             </div>
             <x-form.textarea name="excerpt">{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
             <x-form.textarea name="body">{{ old('body', $post->body) }}</x-form.textarea>
